da863e07c18dde857b8c0775d31ae0d95b1aa36f
Veranika Kasparevych <veranikabarel@gmail.com>
2022-05-29 16:12:25 +0200

    feat: add create blog fuctionality

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,48 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Create = () => {
-    return ( 
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+    const history = useHistory();
+
+    const submitBlog = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+        fetch('http://localhost:8000/blogs/', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            history.push('/');
+        })
+    }
+
+
+
+    return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form onSubmit={submitBlog}>
+                <label>Blog title:</label>
+                <input type='text' required value={title} onChange={(e) => setTitle(e.target.value)}></input>
+                <label>Blog body:</label>
+                <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+                <label>Blog author:</label>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <option value='mario'>mario</option>
+                    <option value='yoshi'>yoshi</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
-     );
+    );
 }
- 
+
 export default Create;
